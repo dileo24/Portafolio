872ddbf99987265a569ea6c7a000c94ca3482065
dileo24 <joacodileo@gmail.com>
2023-01-13 04:48:06 -0300

    email enviado

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
 /* MENÚ */
 ((d) => {
-    const $btnMenu = d.querySelector(".menu-btn")
-    const $menu = d.querySelector(".menu")
+  const $btnMenu = d.querySelector(".menu-btn");
+  const $menu = d.querySelector(".menu");
 
-    $btnMenu.addEventListener("click", (e) => {
-        $btnMenu.firstElementChild.classList.toggle("none")
-        $btnMenu.lastElementChild.classList.toggle("none")
-        $menu.classList.toggle("is-active")
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+    //si el evento es un enlace que esté dentro del menú se va a cerrar
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
+
+/*CONTACT FROM*/
+/* (d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/joacodileo@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
     })
-
-    d.addEventListener("click", (e) => {
-        if (!e.target.matches(".menu a")) return false;
-        //si el evento es un enlace que esté dentro del menú se va a cerrar
-        $btnMenu.firstElementChild.classList.remove("none")
-        $btnMenu.lastElementChild.classList.add("none")
-        $menu.classList.remove("is-active")
-    })
-
-})(document)
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let msg = err.statusText || "Error al enviar, intenta nuevamente!";
+        $response.querySelector("h3").innerHTML = `Error ${err.status}: ${msg}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
+},
+  document;
+ */
